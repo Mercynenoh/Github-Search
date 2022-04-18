2c91868babb5de9b2d9876d5e750a479436cd2c6
Mercynenoh <mercychepngenoh74@gmail.com>
2022-04-19 00:11:04 +0300

    add new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
-
 import { NotFoundComponent } from './not-found/not-found.component';
-
+import { RepoComponent } from './repo/repo.component';
 const routes: Routes = [
   { path: 'gitsearch', component: AppComponent},
-  
+  { path: 'repo', component: RepoComponent},
   { path: '', redirectTo:"/gitsearch", pathMatch:"full"},
   { path:'**', component:NotFoundComponent},
 
